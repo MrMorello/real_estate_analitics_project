cd9fcf5a376c787f8862d10ec51c4b1f9c8f7070
MrMorello <50555305+MrMorello@users.noreply.github.com>
2019-10-18 11:58:30 +0300

    + graphics

--- data_aggregation.py
+++ data_aggregation.py
@@ -1,33 +1,31 @@
 import pandas as pd
-#import numpy as np
+import numpy as np
+from matplotlib import pyplot as plt
+import matplotlib.image as mpimg
 
 df_adv = pd.read_excel('D:\_Machine Learning\gipernn_adv.xlsx', index_col=0)
-df_geo = pd.read_excel('D:\_Machine Learning\housing_DB_lat_long.xlsx', index_col=0)
-
-adv_np = df_adv.to_numpy()
-geo_np = df_geo.to_numpy()
-adv_longtitude = []
-adv_latitude = []
-
-for index, row in df_adv.iterrows():
-    row['address'] = row['address'].replace(' д.', '')
-    row['address'] = row['address'].split(" ", maxsplit=1)[1:][0]
-    print(row['address'])
-
-for index_adv, row_adv in df_adv.iterrows():
-    for index_geo, row_geo in df_geo.iterrows():
-        if row_adv['address'] == row_geo['addres']:
-            adv_longtitude.append(row_geo['longtitude'])
-            adv_latitude.append(row_geo['latitude'])
-
-        else:
-            pass
-            
-        print(len(adv_latitude))
-        print(len(adv_longtitude))
+df_adv['mean_price_sqrm'] = df_adv['price'] / df_adv['total_square']
+housing = df_adv.copy()
 
 
 
+california_img=mpimg.imread('images\california2.png')
+ax = housing.plot(kind="scatter", x="longtitude", y="latitude", figsize=(10, 7),
+                       s=3, label="Objects",
+                       c=housing['mean_price_sqrm'], cmap=plt.get_cmap("jet"),
+                       colorbar=False, alpha=0.4)
+plt.imshow(california_img, extent=[43.6, 44.1, 56.15, 56.4], alpha=0.8,
+           cmap=plt.get_cmap("jet"))
+plt.ylabel("latitude", fontsize=14)
+plt.xlabel("longtitude", fontsize=14)
 
-print(df_adv.info())
-print(df_geo.info())
+prices = housing["mean_price_sqrm"]
+tick_values = np.linspace(prices.min(), prices.max(), 11)
+#cbar = plt.colorbar()
+#cbar.ax.set_yticklabels(["$%dk"%(round(v/1000)) for v in tick_values], fontsize=14)
+#cbar.set_label('mean price sqrm', fontsize=16)
+
+plt.legend(fontsize=16)
+plt.savefig("images\colorized_map_plot", dpi=400)
+plt.show()
+
