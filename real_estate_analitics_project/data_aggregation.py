c645e8786921cd63768fc1701de8a642df2bdd19
MrMorello <50555305+MrMorello@users.noreply.github.com>
2019-10-22 15:42:18 +0300

    in process

--- real_estate_analitics_project/data_aggregation.py
+++ real_estate_analitics_project/data_aggregation.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
+from sklearn.model_selection import train_test_split
+
 
 #pd.options.display.max_rows = 20
 #pd.options.display.max_columns = 20
@@ -10,6 +12,12 @@ df_adv = pd.read_excel('tables\gipernn_adv.xlsx', index_col=0)
 df_adv['mean_price_sqrm'] = df_adv['price'] / df_adv['total_square']
 housing = df_adv.copy()
 
+housing['build_year'] = housing['build_year'].replace('Уточнить', None)
+housing['build_year'] = housing['build_year'].astype('float64', errors='ignore')
+housing['current_floor'] = housing['current_floor'].replace('цоколь ', 0)
+housing['current_floor'] = housing['current_floor'].astype('float64')
+housing['ceiling_height'] = housing['ceiling_height'].replace('Уточнить', None)
+housing['ceiling_height'] = housing['ceiling_height'].astype('float64')
 
 california_img=mpimg.imread('images\california2.png')
 ax = housing.plot(kind="scatter", x="longtitude", y="latitude", figsize=(10, 7),
@@ -29,8 +37,22 @@ housing.hist(bins=50, figsize=(20,15))
 plt.savefig('images\_02attribute_histogram_plots')
 plt.show()
 
-housing['build_year'] = housing['build_year'].replace('Уточнить', 0)
-#pd.to_numeric(housing['build_year'])
-housing['build_year'] = housing['build_year'].astype('float64', errors='ignore')
-print(housing.info())
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+# add code from page 86
+'''
+1. данный блок вставить перед вырисовыванием графиков
+стратифицированный набор:
+from sklearn.model_selection import StratifiedShuffleSplit   /* page 85
+
+2. После графиков
+Поиск связей .corr() 
+from pandas.plotting import scatter_matrix
+3. Создание снтетических признаков и эксперименты с ними
+4. Сброс целевой переменной (price)
+5. Очистка данных:
+5.1 
+'''
+
+
+
 
